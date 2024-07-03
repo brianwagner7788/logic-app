7ca59b25b0d7a47f3bdcc6902dc7c0bc19790afc
brianwagner7788 <brian.wagner7788@outlook.com>
2024-07-03 18:21:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "azurerm_logic_app_standard" "logic_app" {
   name                       = var.name
   location                   = var.location
   resource_group_name        = var.resource_group_name
+  app_service_plan_id        = var.app_service_plan_id
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
 }
