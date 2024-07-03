347f2f6c9d58839f3163eee02e38a809fb3271e9
brianwagner7788 <brian.wagner7788@outlook.com>
2024-07-03 17:05:16 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
 variable "name" {
-  description = "Some description"
-  type        = "string"
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage_account" {
+  type = object({
+    name                = string
+    resource_group_name = string
+    primary_access_key  = string
+  })
 }
